609a16b6e96dd10d10a8784a289403fa18ad9267
Daniel Fernandez <xsteguin@gmail.com>
2021-07-06 23:00:08 -0500

    Hangman game - Course project

---
+++ hangman_game.py
@@ -0,0 +1,12 @@
+"""
+REQUISITOS
+    Limpiar la pantalla.
+    Adivina la palabra.
+    Ingresar una letra.
+    Actualizar lista.
+    Mostrar mensaje de ganar.
+    Comprehensions, errores, archivos.
+    Usar archivo data.txt
+    Enumerate, get, os.system("clear").
+    Sistema de puntos, dibujo ASCII, mejora interfaz. 
+"""
